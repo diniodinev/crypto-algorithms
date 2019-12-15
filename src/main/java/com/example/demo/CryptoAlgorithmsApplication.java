538ad435ebc0657c9882367aab01c31764952e6d
Dinyo Ilchev Dinev <dinio.dinev@gmail.com>
2019-12-15 21:12:26 +0200

    Init commit

---
+++ src/main/java/com/example/demo/CryptoAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoAlgorithmsApplication.class, args);
+	}
+
+}
